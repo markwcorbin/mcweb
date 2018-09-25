3890fa91d0d99567478534cb37469f88bc9f6710
markwcorbin <34870054+markwcorbin@users.noreply.github.com>
2018-09-24 19:47:38 -0500

    Fixed model bugs
    There was a problem with the workout datetime field def -- didn't code it as an instantiation.

--- inshape/models.py
+++ inshape/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Routine(models.Model):
     name = models.CharField(max_length=20)
@@ -21,7 +22,7 @@ class Exercise(models.Model):
         return self.name
 
 class Workout(models.Model):
-    workout_date = models.DateTimeField
+    workout_date = models.DateTimeField(default=datetime.now())
     routine = models.IntegerField(default=0)
     notes = models.CharField(max_length=100)
 
